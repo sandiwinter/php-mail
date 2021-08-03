fd554e444d895b50822e4f5236d5f8ce4780b643
Sandi <sandi.winter@gmail.com>
2021-08-03 10:52:07 +0200

    for PHP >= 7.2

--- mail_send.php
+++ mail_send.php
@@ -2987,7 +2987,7 @@ class SMTP {
 
     $max_line_length = 998; // used below; set here for ease in change
 
-    while(list(,$line) = @each($lines)) {
+    foreach ($lines as $key => $line) {
       $lines_out = null;
       if($line == "" && $in_headers) {
         $in_headers = false;
@@ -3016,7 +3016,7 @@ class SMTP {
       $lines_out[] = $line;
 
       // send the lines to the server
-      while(list(,$line_out) = @each($lines_out)) {
+      foreach ($lines_out as $line_out) {
         if(strlen($line_out) > 0)
         {
           if(substr($line_out, 0, 1) == ".") {
